3ac279db76420cdc4e672fd01174171fdbd29474
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-06-26 23:16:20 -0400

    integration progress between main & functionality classes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,11 +1,58 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+    static String powerOffPrompt = "Calculator powering off...";
+
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
+        Console.println("Welcome to my calculator!".toUpperCase());
+
+        String input;
+        boolean activeCalc = true;
+        String d = "d";
+        String m = "m";
+
+
+
+        MainApplication mainCalc = new MainApplication();
+        Calculator coreCalc = new Calculator();
+        ScientificFts scientificFeats = new ScientificFts();
+
+
+        mainCalc.displayMenu();
+
+        while (activeCalc) {
+            input = mainCalc.userInput();
+
+            switch (input) {
+                case "d":
+                    System.out.println("You entered " + d + "\n");
+                    scientificFeats.switchDisplayMode();
+                    break;
+                case "m":
+                    System.out.println("You entered " + m + "\n");
+                    break;
+                case "+":
+                    System.out.println("Addition happening");
+                    coreCalc.add();
+                case "q":
+                    System.out.println(powerOffPrompt);
+                    activeCalc = false;
+                    exit(5);
+                default:
+                    throw new IllegalStateException("Unexpected value: " + input);
+            }
+        }
+
+
+
+/*
         String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
         Double d = Console.getDoubleInput("Enter a double.");
@@ -13,13 +60,14 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+*/
 
 
 
     }
 
     public void displayMenu() {
-        String header = "execute scientific features\n";
+//        String header = "execute scientific features\n";
         String separator = "************************************************\n";
 
         String switchDisplay = "d: cycle display modes(binary, octal, decimal, hexadecimal)\n";
@@ -30,11 +78,21 @@ public class MainApplication {
         String multiplication = "*: begin multiplication calculation\n";
         String quit = "q: quit";
 
-        String menu = (header.toUpperCase() + separator +
+        String menu = (separator +
                 switchDisplay + storeValMemory + trigFunctionality +
                 addition + subtraction + multiplication +
                 quit);
         System.out.println(menu);
     }
+
+    public String userInput() {
+        String inputPrompt = "Enter letter to select feature: ";
+        System.out.print(inputPrompt);
+        Scanner sc = new Scanner(System.in);
+        String nextMode = sc.nextLine();
+
+        return nextMode;
+    }
+
 
 }
